1575547431994be678a159fd2e0b1d875ac0383e
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-04-09 23:58:44 -0400

    completed

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -6,3 +6,15 @@ func _ready():
 func _unhandled_input(event):
 	if Input.is_action_pressed("Quit"):
 		get_tree().quit()
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_Nearby_area_exited(area):
+	pass # Replace with function body.
+
+
+func _on_Nearby_area_entered(area):
+	pass # Replace with function body.
